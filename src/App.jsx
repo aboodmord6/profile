4c48b5a06857269c9c4aa4633bdc9144aa58480e
abdallah <aboodmord6@gmail.com>
2026-01-06 13:09:57 +0300

    feat: Add initial application structure and integrate Vercel analytics and speed insights.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Analytics />
+      <SpeedInsights />
     </>
   )
 }
